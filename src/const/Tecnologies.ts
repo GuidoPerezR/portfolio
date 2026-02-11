ae13f39e999210e834b93fef7cfa0e20d9c89b59
Guido Perez Ruiz <guidopr27@hotmail.com>
2026-02-10 19:35:34 -0600

    feat: add HTML and CSS icon components and define TECHNOLOGIES constant

---
+++ src/const/Tecnologies.ts
@@ -0,0 +1,45 @@
+import HTML from '@/components/icons/HTML.astro';
+import Css from '@/components/icons/Css.astro';
+
+export const TECHNOLOGIES = [
+  {
+    name: 'HTML',
+    icon: HTML,
+    color: '#E34F26',
+  },
+  {
+    name: 'CSS',
+    icon: Css,
+    color: '#3178C6',
+  },
+  {
+    name: 'Tailwind CSS',
+    icon: 'tailwindcss',
+    color: '#3178C6',
+  },
+  {
+    name: 'JavaScript',
+    icon: 'javascript',
+    color: '#F7DF1E',
+  },
+  {
+    name: 'TypeScript',
+    icon: 'typescript',
+    color: '#3178C6',
+  },
+  {
+    name: 'React',
+    icon: 'react',
+    color: '#61DAFB',
+  },
+  {
+    name: 'Astro',
+    icon: 'astro',
+    color: '#FF5D01',
+  },
+  {
+    name: 'Git',
+    icon: 'git',
+    color: '#F05032',
+  },
+];
