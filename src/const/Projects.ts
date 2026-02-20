9d726cd915a6f720e177046b82716dd2aec6d9e4
Guido Perez Ruiz <guidopr27@hotmail.com>
2026-02-19 18:31:21 -0600

    feat: Add English and Spanish versions for projects page

--- src/const/Projects.ts
+++ src/const/Projects.ts
@@ -1,73 +1,148 @@
-export const PROJECTS = [
-  {
-    id: 'project-1',
-    title: 'Manjares',
-    short_desc:
-      'Sitio web inspirada en la cafetería Manjares Panadería Fina, un espacio que celebra la panadería artesanal y el café de calidad.',
-    long_desc:
-      'Proyecto que me permitió sentar las bases de la programación web, mejorando la semántica, el diseño y la interactividad de mis proyectos. Sitio diseñado para ofrecer una experiencia cálida, elegante y visualmente atractiva.',
-    date: '28/04/25',
-    image: '/images/manjares.webp',
-    technologies: ['HTML', 'CSS', 'JavaScript'],
-    link: 'https://guidoperezr.github.io/cafeteria-landing-page/',
-    github: 'https://github.com/GuidoPerezR/cafeteria-landing-page',
-  },
-  {
-    id: 'project-2',
-    title: 'Luminari',
-    short_desc:
-      'Landing page inspirada en una galería de arte moderna, diseñada para ofrecer una experiencia visual innovadora',
-    long_desc:
-      'Sitio web desarrollado con un enfoque en la experiencia del usuario, implementando movimientos parallax, animación con scroll horizontal y animaciones dinámicas con GSAP.',
-    date: '07/08/25',
-    image: '/images/luminari.webp',
-    technologies: ['Astro', 'Tailwind CSS', 'JavaScript'],
-    link: 'https://luminari-gallery.vercel.app/',
-    github: 'https://github.com/GuidoPerezR/luminari',
-  },
-  {
-    id: 'project-3',
-    title: 'Nahual Studio',
-    short_desc:
-      'Landing page para el estudio de diseño gráfico de Nahual Studio, centralizando toda la información del cliente en un sitio moderno y atractivo.',
-    long_desc:
-      'Implementación de una aplicación web para aumentar la visualización del estudio y centralizando toda la información del cliente en un solo lugar. Desarrollo de interfaces atractivas, animaciones simples y un diseño que pueda representar a la marca.',
-    date: '25/11/25',
-    image: '/images/nahual-studio.webp',
-    technologies: ['Astro', 'Tailwind CSS', 'JavaScript'],
-    link: 'https://nahual-studio.vercel.app/',
-    github: 'https://github.com/GuidoPerezR/nahual-studio',
-  },
-  {
-    id: 'project-4',
-    title: 'Cars Collection',
-    short_desc:
-      'Sitio web sobre mi coleccion personal de autos de Cars, para registrar y guardar toda la informacion de mis autos en un solo lugar.',
-    long_desc:
-      'Sitio web realizado con React.js para empezar a entender su funcionamiento, usando estados, efectos, Custom Hooks, filtros consumiendo contexto con un estado global y enrutamiento con React Router. Integración de Strapi para la gestión de datos.',
-    date: '11/12/25',
-    image: '/images/cars-collection.webp',
-    technologies: ['React', 'Tailwind CSS', 'Strapi'],
-    link: 'https://widos-garage.vercel.app/',
-    github: 'https://github.com/GuidoPerezR/cars-collection',
-  },
-  {
-    id: 'project-5',
-    title: 'Saving Saves',
-    short_desc:
-      'Una aplicación web para gestionar tus finanzas personales de forma facil y sencilla.',
-    long_desc:
-      'Esta aplicación te permite registrar tus gastos e ingresos para poder llevar una buena salud financiera, ademas de poder visualizar tu historial de finanzas de manera clara y organizada. Estado global con Zustand, integracion de autenticacion y base de datos con Supabase.',
-    date: '03/02/26',
-    image: '/images/savings-app.webp',
-    technologies: [
-      'React',
-      'Tailwind CSS',
-      'TypeScript',
-      'Supabase',
-      'Valibot',
-    ],
-    link: 'https://savingsaves.vercel.app/',
-    github: 'https://github.com/GuidoPerezR/savings-app',
-  },
-];
+export const PROJECTS = {
+  es: [
+    {
+      id: 'project-1',
+      title: 'Manjares',
+      short_desc:
+        'Sitio web inspirada en la cafetería Manjares Panadería Fina, un espacio que celebra la panadería artesanal y el café de calidad.',
+      long_desc:
+        'Proyecto que me permitió sentar las bases de la programación web, mejorando la semántica, el diseño y la interactividad de mis proyectos. Sitio diseñado para ofrecer una experiencia cálida, elegante y visualmente atractiva.',
+      date: '28/04/25',
+      image: '/images/manjares.webp',
+      technologies: ['HTML', 'CSS', 'JavaScript'],
+      link: 'https://guidoperezr.github.io/cafeteria-landing-page/',
+      github: 'https://github.com/GuidoPerezR/cafeteria-landing-page',
+    },
+    {
+      id: 'project-2',
+      title: 'Luminari',
+      short_desc:
+        'Landing page inspirada en una galería de arte moderna, diseñada para ofrecer una experiencia visual innovadora',
+      long_desc:
+        'Sitio web desarrollado con un enfoque en la experiencia del usuario, implementando movimientos parallax, animación con scroll horizontal y animaciones dinámicas con GSAP.',
+      date: '07/08/25',
+      image: '/images/luminari.webp',
+      technologies: ['Astro', 'Tailwind CSS', 'JavaScript'],
+      link: 'https://luminari-gallery.vercel.app/',
+      github: 'https://github.com/GuidoPerezR/luminari',
+    },
+    {
+      id: 'project-3',
+      title: 'Nahual Studio',
+      short_desc:
+        'Landing page para el estudio de diseño gráfico de Nahual Studio, centralizando toda la información del cliente en un sitio moderno y atractivo.',
+      long_desc:
+        'Implementación de una aplicación web para aumentar la visualización del estudio y centralizando toda la información del cliente en un solo lugar. Desarrollo de interfaces atractivas, animaciones simples y un diseño que pueda representar a la marca.',
+      date: '25/11/25',
+      image: '/images/nahual-studio.webp',
+      technologies: ['Astro', 'Tailwind CSS', 'JavaScript'],
+      link: 'https://nahual-studio.vercel.app/',
+      github: 'https://github.com/GuidoPerezR/nahual-studio',
+    },
+    {
+      id: 'project-4',
+      title: 'Cars Collection',
+      short_desc:
+        'Sitio web sobre mi coleccion personal de autos de Cars, para registrar y guardar toda la informacion de mis autos en un solo lugar.',
+      long_desc:
+        'Sitio web realizado con React.js para empezar a entender su funcionamiento, usando estados, efectos, Custom Hooks, filtros consumiendo contexto con un estado global y enrutamiento con React Router. Integración de Strapi para la gestión de datos.',
+      date: '11/12/25',
+      image: '/images/cars-collection.webp',
+      technologies: ['React', 'Tailwind CSS', 'Strapi'],
+      link: 'https://widos-garage.vercel.app/',
+      github: 'https://github.com/GuidoPerezR/cars-collection',
+    },
+    {
+      id: 'project-5',
+      title: 'Saving Saves',
+      short_desc:
+        'Una aplicación web para gestionar tus finanzas personales de forma facil y sencilla.',
+      long_desc:
+        'Esta aplicación te permite registrar tus gastos e ingresos para poder llevar una buena salud financiera, ademas de poder visualizar tu historial de finanzas de manera clara y organizada. Estado global con Zustand, integracion de autenticacion y base de datos con Supabase.',
+      date: '03/02/26',
+      image: '/images/savings-app.webp',
+      technologies: [
+        'React',
+        'Tailwind CSS',
+        'TypeScript',
+        'Supabase',
+        'Valibot',
+      ],
+      link: 'https://savingsaves.vercel.app/',
+      github: 'https://github.com/GuidoPerezR/savings-app',
+    },
+  ],
+  en: [
+    {
+      id: 'project-1',
+      title: 'Manjares',
+      short_desc:
+        'Website inspired by the Manjares Fine Bakery, a space that celebrates artisanal baking and quality coffee.',
+      long_desc:
+        'Project that allowed me to lay the foundations of web programming, improving semantics, design, and interactivity of my projects. Site designed to offer a warm, elegant, and visually appealing experience.',
+      date: '04/28/25',
+      image: '/images/manjares.webp',
+      technologies: ['HTML', 'CSS', 'JavaScript'],
+      link: 'https://guidoperezr.github.io/cafeteria-landing-page/',
+      github: 'https://github.com/GuidoPerezR/cafeteria-landing-page',
+    },
+    {
+      id: 'project-2',
+      title: 'Luminari',
+      short_desc:
+        'Landing page inspired by a modern art gallery, designed to offer an innovative visual experience.',
+      long_desc:
+        'Website developed with a focus on user experience, implementing parallax movements, horizontal scroll animation, and dynamic animations with GSAP.',
+      date: '08/07/25',
+      image: '/images/luminari.webp',
+      technologies: ['Astro', 'Tailwind CSS', 'JavaScript'],
+      link: 'https://luminari-gallery.vercel.app/',
+      github: 'https://github.com/GuidoPerezR/luminari',
+    },
+    {
+      id: 'project-3',
+      title: 'Nahual Studio',
+      short_desc:
+        'Landing page for the Nahual Studio graphic design studio, centralizing all client information in a modern and attractive site.',
+      long_desc:
+        'Implementation of a web application to increase the visibility of the studio and centralizing all customer information in one place. Development of attractive interfaces, simple animations, and a design that can represent the brand.',
+      date: '11/25/25',
+      image: '/images/nahual-studio.webp',
+      technologies: ['Astro', 'Tailwind CSS', 'JavaScript'],
+      link: 'https://nahual-studio.vercel.app/',
+      github: 'https://github.com/GuidoPerezR/nahual-studio',
+    },
+    {
+      id: 'project-4',
+      title: 'Cars Collection',
+      short_desc:
+        'Website about my personal Cars car collection, to register and save all information about my cars in one place.',
+      long_desc:
+        'Website built with React.js to start understanding how it works, using states, effects, Custom Hooks, filters consuming context with a global state, and routing with React Router. Integration with Strapi for data management.',
+      date: '12/11/25',
+      image: '/images/cars-collection.webp',
+      technologies: ['React', 'Tailwind CSS', 'Strapi'],
+      link: 'https://widos-garage.vercel.app/',
+      github: 'https://github.com/GuidoPerezR/cars-collection',
+    },
+    {
+      id: 'project-5',
+      title: 'Saving Saves',
+      short_desc:
+        'A web application to manage your personal finances easily and simply.',
+      long_desc:
+        'This application allows you to record your expenses and income to maintain healthy finances, as well as visualize your financial history clearly and neatly. Global state with Zustand, authentication integration, and database with Supabase.',
+      date: '02/03/26',
+      image: '/images/savings-app.webp',
+      technologies: [
+        'React',
+        'Tailwind CSS',
+        'TypeScript',
+        'Supabase',
+        'Valibot',
+      ],
+      link: 'https://savingsaves.vercel.app/',
+      github: 'https://github.com/GuidoPerezR/savings-app',
+    },
+  ],
+};
