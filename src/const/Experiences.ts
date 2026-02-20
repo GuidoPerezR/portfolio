8fb44a3a578a0ed725bca2de233fa2aaa3384f2a
Guido Perez Ruiz <guidopr27@hotmail.com>
2026-02-19 18:30:45 -0600

    feat: Create internationalized experience page

--- src/const/Experiences.ts
+++ src/const/Experiences.ts
@@ -1,19 +1,38 @@
-export const EXPERIENCES = [
-  {
-    id: 'exytur',
-    name: 'Exytur',
-    role: 'Desarrollador Full Stack',
-    period: 'Noviembre 2023 - Marzo 2025',
-    description:
-      'Desarrollo de una aplicación web desde cero para automatizar y optimizar el registro de las ventas diarias de la empresa. Implementación de reportes diarios de las ventas en PDF. Diseño de formularios e interfaces para la gestión y visualización de la información de forma rápida y eficiente. Automatización del procedimiento de ventas en un 60%.',
-  },
-  {
-    id: 'upt',
-    name: 'Universidad Politécnica de Tapachula',
-    role: 'Desarrollador Full Stack',
-    period: 'Agosto 2023 - Agosto 2024',
-    description:
-      'Contribuí al desarrollo del sistema interno de una universidad, creando RESTful APIs para la gestión de datos de estudiantes y profesores. Implementé interfaces dinámicas para facilitar la consulta de información. Genere y optimice reportes anuales en Excel en un 40%, mejorando consultas SQL para obtener datos de manera más eficiente.',
-    technologies: ['Astro', 'TypeScript'],
-  },
-];
+export const EXPERIENCES = {
+  es: [
+    {
+      id: 'exytur',
+      name: 'Exytur',
+      role: 'Desarrollador Full Stack',
+      period: 'Noviembre 2023 - Marzo 2025',
+      description:
+        'Desarrollo de una aplicación web desde cero para automatizar y optimizar el registro de las ventas diarias de la empresa. Implementación de reportes diarios de las ventas en PDF. Diseño de formularios e interfaces para la gestión y visualización de la información de forma rápida y eficiente. Automatización del procedimiento de ventas en un 60%.',
+    },
+    {
+      id: 'upt',
+      name: 'Universidad Politécnica de Tapachula',
+      role: 'Desarrollador Full Stack',
+      period: 'Agosto 2023 - Agosto 2024',
+      description:
+        'Contribuí al desarrollo del sistema interno de una universidad, creando RESTful APIs para la gestión de datos de estudiantes y profesores. Implementé interfaces dinámicas para facilitar la consulta de información. Genere y optimice reportes anuales en Excel en un 40%, mejorando consultas SQL para obtener datos de manera más eficiente.',
+    },
+  ],
+  en: [
+    {
+      id: 'exytur',
+      name: 'Exytur',
+      role: 'Full Stack Developer',
+      period: 'November 2023 - March 2025',
+      description:
+        'Development of a web application from scratch to automate and optimize the daily sales records of the company. Implementation of daily sales reports in PDF. Design of forms and interfaces for fast and efficient data management and visualization. Automated the sales process by 60%.',
+    },
+    {
+      id: 'upt',
+      name: 'Universidad Politécnica de Tapachula',
+      role: 'Full Stack Developer',
+      period: 'August 2023 - August 2024',
+      description:
+        'Contributed to the development of an internal university system, creating RESTful APIs for student and teacher data management. Implemented dynamic interfaces to improve getting information. Generated and optimized annual Excel reports by 40%, improving SQL queries to fetch data more efficiently.',
+    },
+  ],
+};
