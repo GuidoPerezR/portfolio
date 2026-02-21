fd8dbbcc8cf9ad0a0cfb9679b08df4b3296a0a5e
Guido Perez Ruiz <guidopr27@hotmail.com>
2026-02-20 20:18:24 -0600

    fix: fix url

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'guidzero.dev',
+  site: 'https://guidzero.dev',
   vite: {
     plugins: [tailwindcss()],
   },
